bfe7b41c128560194accfde29ebfe55119b09c15
Eduarduar <eduardoarcega1997@gmail.com>
2024-02-09 20:36:13 -0600

    se aplico el footer al index/home

--- index.php
+++ index.php
@@ -3,16 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/997c58a28f.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://startbootstrap.com/templates/agency/font-awesome-4.1.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/style_navBar.css">
+    <link rel="stylesheet" href="./css/style_footer.css">
     <title>Document</title>
 </head>
 <body id="page-top">
 
     <!-- Navigation -->
     <?php include 'views/navBar.php'; ?>
+
+    
+
+    <!-- Footer -->
+
+    <?php include 'views/footer.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
---
+++ views/footer.php
@@ -0,0 +1,42 @@
+
+<footer class="footer">
+    <div class="col-1">
+        <h3>ABOUT</h3>
+            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming <br>
+                programmers with the code. Scanfcode focuses on providing the most efficient<br>
+                code or snippets as the code wants to be simple. We will help programmers build<br>
+                up concepts in different programming languages that include C, C++, Java, HTML,<br>
+                Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+            <br>
+                <br> <br>
+                <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+                    <a href="#">Scanfcode</a>.
+                </p>
+        
+    
+    </div>
+    <div class="col-2">
+        <h3>CATEGORIES</h3>
+            <a href="#">C</a>
+            <a href="#">UI Desing</a>
+            <a href="#">PHP</a>
+            <a href="#">Java </a>
+            <a href="#">Android </a>
+            <a href="#">Templates</a>
+    </div>
+    <div class="col-3">
+        <h3>QUICK LINKS</h3>
+            <a href="#">About Us </a>
+            <a href="#">Contact Us</a>
+            <a href="#">Contribute</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Sitemap </a>
+        <div class="social-icons">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-instagram"></i>
+        </div>
+
+    </div>
+
+</footer>
