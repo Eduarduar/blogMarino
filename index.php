185e4ffa7c52925199fe41fce8941ceeebb99fdb
Hydra314 <ggutierrez0@ucol.mx>
2024-02-11 18:28:45 -0600

    se agrego el carrusel

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://startbootstrap.com/templates/agency/font-awesome-4.1.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/style_navBar.css">
+    <link rel="stylesheet" href="./css/style_carrusel.css">
     <link rel="stylesheet" href="./css/style_footer.css">
     <title>Document</title>
 </head>
@@ -23,7 +24,29 @@
     <!-- Navigation -->
     <?php include 'views/navBar.php'; ?>
 
-    
+    <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1"></li>
+            <li data-target="#carousel" data-slide-to="2"></li>
+            <li data-target="#carousel" data-slide-to="3"></li>
+            <li data-target="#carousel" data-slide-to="4"></li>
+            <li data-target="#carousel" data-slide-to="5"></li>
+        </ol>
+        <!-- Carousel items -->
+        <div class="carousel-inner">
+            <div class="item active"><img src="./source/img/imagen1.jpg" alt=""></div>
+            <div class="item"><img src="./source/img/imagen2.jpg" alt=""></div>
+            <div class="item"><img src="./source/img/imagen3.jpg" alt=""></div>
+            <div class="item"><img src="./source/img/imagen4.jpg" alt=""></div>
+            <div class="item"><img src="./source/img/imagen5.jpg" alt=""></div>
+            <div class="item"><img src="./source/img/imagen6.webp" alt=""></div>
+        </div>
+        <!-- Carousel nav -->
+        <a class="carousel-control left" href="#carousel" id="prev" data-slide="prev">&lsaquo;</a>
+        <a class="carousel-control right" href="#carousel" id="next" data-slide="next">&rsaquo;</a>
+    </div>
+
 
     <!-- Footer -->
 
@@ -32,6 +55,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="./js/navBar.js"></script>
+    <script src="./js/index.js"></script>
 
 </body>
 </html>
