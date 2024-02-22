60c3bd230f35335cc932b8718c5b86d0cb016a41
Eduarduar <eduardoarcega1997@gmail.com>
2024-02-21 18:05:48 -0600

    se modifico el footer

--- views/footer.php
+++ views/footer.php
@@ -1,18 +1,18 @@
 
 <footer class="footer">
     <div class="col-1">
-        <h3>ABOUT</h3>
-            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming <br>
+        <!-- <h3>ABOUT</h3> -->
+            <!-- <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming <br>
                 programmers with the code. Scanfcode focuses on providing the most efficient<br>
                 code or snippets as the code wants to be simple. We will help programmers build<br>
                 up concepts in different programming languages that include C, C++, Java, HTML,<br>
-                Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
-            <br>
-                <br> <br>
+                Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p> -->
+            <!-- <br>
+                <br> <br> -->
                 <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
                     <a href="#">Scanfcode</a>.
                 </p>
-        
+<!--         
     
     </div>
     <div class="col-2">
@@ -37,6 +37,6 @@
             <i class="fa-brands fa-instagram"></i>
         </div>
 
-    </div>
+    </div> -->
 
 </footer>
