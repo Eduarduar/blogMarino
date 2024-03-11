0ab29c8cf5135c4905d3001a0146cf5b08796e1e
ELNARMOTON79 <rafaelalexandro6949@gmail.com>
2024-03-10 20:15:18 -0600

    Se agrego prototipo del formulario

--- manage/addCategory.php
+++ manage/addCategory.php
@@ -1,5 +1,7 @@
 <?php
-
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    
     session_start();
 
     if (!isset($_SESSION['idUser'])) {
@@ -12,6 +14,7 @@
 
     $UserName = $contacto->getUserName($_SESSION['idUser']);
 
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,14 +24,19 @@
     <link rel="stylesheet" href="../css/style_SideBar.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css">
     <link rel="stylesheet" href="../css/style_fonts.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Manage - DEEP OCEAN</title>
 </head>
 <body>
 
     <?php include '../views/menu_manage.php'; ?>
+    <form class="flex flex-col space-y-4">
+        <label for="categoria">Ingrese el nombre de la categoría:</label>
+        <input type="text" name="categoria" id="categoria" class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
 
-    
-
+        
+    </form>
+    <input type="submit" name='crear_cat' value="Agregar" id = "crear_cat" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
     <?php include '../views/footer_manage.php'; ?>
 
             
