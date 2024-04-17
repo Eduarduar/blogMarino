08e1d46eee367c5479ae0249f294a3ace218f38a
Eduarduar <eduardoarcega1997@gmail.com>
2024-04-17 11:31:15 -0600

    Update: Se agrego el diseño del apartado de recuperación de contraseña

--- recovery.php
+++ recovery.php
@@ -8,10 +8,10 @@ if (isset($_SESSION['idUser'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title>Log In</title>
+    <title>Recovery Password</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="./source/library/jquery/jquery-3.6.0.min.js"></script> 
     <script src="./source/library/fontawesome-free-5.15.4-web/js/all.min.js"></script>
@@ -25,23 +25,15 @@ if (isset($_SESSION['idUser'])) {
     </div>
     <div class="login-box">
       <div class="exit-container">
-        <a href="./">
+        <a href="./login">
           <b class="fas fa-times sm:text-3xl md:text-2x1 "></b>
         </a>
       </div>
-      <h2 class="text-[2.5rem]">Login</h2>
+      <h2 class="text-[2rem]">Recovery Password</h2>
       <form method="POST" action="./login" class="form">
         <div class="user-box">
-          <input type="text" class="user" name="usuario" required="">
-          <label>Username</label>
-        </div>
-        <div class="user-box">
-          <input type="password" class="pass" name="contra" required="">
-          <label>Password</label>
-        </div>
-        <div class="mt-4">
-          <input type="checkbox" class="form-checkbox rounded-full appearance-none border-2 border-gray-300 w-5 h-5 transition-colors duration-300 ease-in-out ml-1" name="accept_terms" required="" style="position: relative; top: 3px;">
-            <label class="ml-2 text-white text-[1rem]">Accept <a target="_blank" href="./source/pdf/termsAndConditions.pdf" class="underline">Terms and Conditions</a></label>
+          <input type="text" class="email" name="email" required="">
+          <label>Email</label>
         </div>
         <div class="container-button">
           <i class="log" href="">
@@ -50,21 +42,19 @@ if (isset($_SESSION['idUser'])) {
             <span></span>
             <span></span>
             <button class="boton-personalizado" name="log_in">
-              Login
+              SEND
             </button> 
           </i>
         </div>
-        <div class="w-full mt-12">
-          <a href="./recovery.php" class="mx-auto text-white text-[1rem] underline">I forgot my password</a></div>
+        <div class="error-box display-none">
+        </div>
       </form>
     </div>  
     <div class="access-box display-none">
-        You have successfully logged in
-    </div>
-    <div class="error-box display-none">
+      We have sent you a recovery email
     </div>
 
-    <script src="./js/login.js"></script>
+    <script src="./js/recovery.js"></script>
     <script src="./js/countVisits.js"></script>
   </body>
 </html>
